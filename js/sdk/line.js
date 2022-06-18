a874bcf048ff6d67161965f813098ba27ba93122
Henry G <github.nry@icloud.com>
2022-06-18 10:27:15 +0100

    Add options to define X and Y axis of Line Chart

--- js/sdk/line.js
+++ js/sdk/line.js
@@ -1,11 +1,14 @@
 class Line extends Widget {
+  fnx;
+  fny;
+  _yLabel = ''
   draw(data) {
     this._chart = LineChart.getChart(data, {
-      x: d => new Date(d['date']),
-      y: d => d['close'],
+      x: this._fnx,
+      y: this._fny,
       width: this._width,
       height: this._height,
-      yLabel: "↑ Daily close ($)",
+      yLabel: this._yLabel,
       color: NV5Common.getColor(0)
     });
     super.draw(data);
@@ -14,7 +17,8 @@ class Line extends Widget {
   init() {
     // get data from WS
     NV5Common.makeRequest(this._endpointObject).then(result => {
-      let dt = this._transformLabels(result.data);
+      let dt = result.data;
+      this._applyOptions();
       this.draw(dt);
       if (result.details !== undefined && result.details.message !== undefined) {
         this.appendInformation(result.details.message);
@@ -23,28 +27,18 @@ class Line extends Widget {
     }).catch(result => this.failure(result));
   }
 
-  updateData (data) {
-    // TODO
+  _applyOptions () {
+    let xKey = (this._options.xAxis !== undefined) ? this._options.xAxis.key : 'date';
+    let xIsDate = ((this._options.xAxis !== undefined && this._options.xAxis.date) || this._options.xAxis === undefined);
+    let yKey = (this._options.yAxis !== undefined) ? this._options.yAxis.key : 'value';
+
+    this._yLabel = (this._options.yAxis !== undefined) ? this._options.yAxis.label : '';
+    this._fnx = d => (xIsDate) ? new Date(d[xKey]) : d[xKey]
+    this._fny = d => d[yKey]
   }
 
-  _transformLabels(data) {
-    if(this._keyMapping !== undefined) {
-
-      try {
-        let dt = {};
-        Object.keys(this._keyMapping).forEach((key) => {
-          dt[this._keyMapping[key].label] = data[key]
-        });
-
-        return dt
-      }
-      catch(err) {
-        this.errorMappingKeys(this._containerId, err.message);
-        return data
-      }
-
-    }
-    return data
+  updateData (data) {
+    // TODO
   }
 
   transformDataToTable(data) {
